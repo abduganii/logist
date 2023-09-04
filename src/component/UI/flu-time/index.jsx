fa03b52afba2d2de45b3d3ac2ddde309b9c68c86
Abdugani Abduqaxxorov <abduganiabdu2@gmail.com>
2023-09-04 16:12:07 +0500

    fixed

--- src/component/UI/flu-time/index.jsx
+++ src/component/UI/flu-time/index.jsx
@@ -1,42 +1,74 @@
 
+import moment from 'moment'
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useGetWindowWidth } from '../../hooks/useGetWindowWith'
 import Container from '../container'
 import cls from "./flytime.module.scss"
 
-export default function FlyTime() {
-  // const [data, setData] = useState();
+const timerFunc = (date) => {
+  let days = 0
+  let hours = 0
+  let minutes = 0
+  let seconds = 0
 
-  // useEffect(() => {
-  //   const dataFetch = async () => {
-  //     const data = await (
-  //       await fetch(
-  //         "https://api.telegram.org/bot5912567868:AAGbkDrOSc-kYqjbVE7UOWfUfzq3fiofwIw/getUpdates"
-  //       )
-  //     ).json();
+  
+  var countdown = setInterval(function() {
+    var now = moment();
+    var diff = endDate.diff(now);
+    var duration = moment.duration(diff);
+    days = duration.days();
+    hours = duration.hours();
+    minutes = duration.minutes();
+    seconds = duration.seconds();
+    
+    if (diff <= 0) {
+      clearInterval(countdown);
+    }
+  }, 1000);
+  
+  return {
+    days, hours, minutes, seconds
+  }
+}
 
-      
-  //     setData(data?.result?.[data?.result?.length - 1]?.message?.text);
-  //   };
-
-  //   dataFetch();
-  // }, []);
-
+export default function FlyTime(date) {
   const widthwindow = useGetWindowWidth()
   const { t } = useTranslation(['common'])
+  const [timer, setTimer] = useState({})
+  
+  useEffect(() => {
+    var endDate = moment('2023-09-10');
+    
+    var countdown = setInterval(function() {
+      var now = moment();
+    var diff = endDate.diff(now);
+    var duration = moment.duration(diff);
+    var days = duration.days();
+    var hours = duration.hours();
+    var minutes = duration.minutes();
+    var seconds = duration.seconds();
+    
+    if (diff <= 0) {
+      clearInterval(countdown);
+    }
+
+    setTimer({days, hours, minutes, seconds})
+  }, 1000);
+    
+  }, [])
   return (
     <div className={cls.FlyTime}>
       <div className={cls.FlyTime__back}>
         <Container >
           <h2 className={cls.FlyTime__title}>{ t("FlyTimeTitle")}</h2>
-        {widthwindow <1540? <h3 className={cls.FlyTime__time}> 18:00:00</h3>:""} 
+        {widthwindow <1540? <h3 className={cls.FlyTime__time}>{timer?.days|| 0}:{timer?.hours|| 0}:{timer?.minutes||0}:{timer?.seconds||0}</h3>:""} 
               <div className={cls.FlyTime__wrap}>
                   <div className={cls.FlyTime__card}>
                       <h3 className={cls.FlyTime__card__title}>{ t("FlyTimeChine")}</h3>
                         <p className={cls.FlyTime__card__text}>{ t("FlyTimeChineText")}</p>
                   </div>      
-                 {widthwindow >1540? <h3 className={cls.FlyTime__time}> 18<span>:</span>00<span>:</span>00</h3>:""}   
+                 {widthwindow >1540? <h3 className={cls.FlyTime__time}> {timer?.days|| 0}:{timer?.hours|| 0}:{timer?.minutes||0}:{timer?.seconds||0}</h3>:""}   
                 <div className={cls.FlyTime__card}>
                       <h3 className={cls.FlyTime__card__title}>{ t("FlyTimeUzbek")}</h3>
                         <p className={cls.FlyTime__card__text}>{ t("FlyTimeUzbekText")}</p>
